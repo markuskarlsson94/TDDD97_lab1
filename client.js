230e77ac911be191d281dda017139ebc7cb19460
Markus Karlsson <markus.karlsson.liu@gmail.com>
2019-01-29 16:47:18 +0100

    Basic login/logout prototyped

--- client.js
+++ client.js
@@ -1,3 +1,21 @@
 function test() {
   alert("hejsan");
 }
+
+var signedIn = false;
+
+window.onload = function() {
+  loadWelcome();
+}
+
+function loadProfile() {
+  var html = document.getElementById("profileView");
+  var cont = document.getElementById("content");
+  cont.innerHTML=html.innerHTML;
+}
+
+function loadWelcome() {
+  var html = document.getElementById("welcomeView");
+  var cont = document.getElementById("content");
+  cont.innerHTML=html.innerHTML;
+}
