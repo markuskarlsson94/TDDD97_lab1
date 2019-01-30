1a9227a79767129ca1b21e0f01ca39cee30a78f8
Markus Karlsson <markus.karlsson.liu@gmail.com>
2019-01-30 13:30:31 +0100

    user stays logged in even if refreshed

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 window.onload = function() {
-  loadWelcome();
+  //loadWelcome();
+  if (userLoggedIn()) {
+    loadProfile();
+  } else {
+    loadWelcome();
+  }
 }
 
 function loadProfile() {
@@ -13,4 +18,23 @@ function loadWelcome() {
   var html = document.getElementById("welcomeView");
   var cont = document.getElementById("content");
   cont.innerHTML=html.innerHTML;
+}
+
+function logIn() {
+  localStorage.setItem("loggedIn","true");
+  loadProfile();
+}
+
+function logOut() {
+  localStorage.setItem("loggedIn","false");
+  loadWelcome();
+}
+
+function userLoggedIn() {
+  var val = localStorage.getItem("loggedIn");
+  if (val == "true") {
+    return true;
+  } else {
+    return false;
+  }
 }
